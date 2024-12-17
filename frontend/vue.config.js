6590a53fc1b191b6c859c84b41a275a5d0e7aa40
root <root@localhost.localdomain>
2024-12-17 18:34:45 +0800

    modify ico and title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3021
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '金石系统演示'
+      return args
+    })
   }
 })
